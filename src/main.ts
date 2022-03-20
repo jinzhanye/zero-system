441a8632c5cddd6d0227b872274b8fa2dc98559c
jinzhanye <jinzhan.ye@foxmail.com>
2022-03-20 16:20:58 +0800

    chore: graphql 请求成功

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 import App from './App.vue'
 import router from './router'
+import { provide, h, createApp } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App).use(router).mount('#app')
+// 与 API 的 HTTP 连接
+const httpLink = createHttpLink({
+  // 你需要在这里使用绝对路径
+  uri: 'http://localhost:3999/graphql'
+})
+
+// 缓存实现
+const cache = new InMemoryCache()
+
+// 创建 apollo 客户端
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App)
+}).use(router)
+  .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,39 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <ul v-if="result && result.users">
+      <li v-for="user of result.users" :key="user.name">
+        {{ user.name }} | {{ user.email }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup () {
+    const { result } = useQuery(gql`
+      query getUsers {
+        users {
+          name
+          email
+        }
+      }
+    `)
+
+    return {
+      result
+    }
   }
 })
 </script>
